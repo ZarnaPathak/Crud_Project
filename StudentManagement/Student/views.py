8b91ce9d4b23c8c7a1e7a279f830f6546ea5abc6
Zarna Pathak <pathakzarna0128@gmail.com>
2024-06-02 16:01:18 +0530

    Student/Insert Operation complete
    Create/Add Student details save in Database

--- StudentManagement/Student/views.py
+++ StudentManagement/Student/views.py
@@ -1,8 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Student
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def add_student(request):
+    if request.method == "POST":
+        print("Added")
+        # fetching Student Inputs
+        stds_roll = request.POST.get("std_roll")        
+        stds_name = request.POST.get("std_name")        
+        stds_email = request.POST.get("std_email")        
+        stds_address = request.POST.get("std_address")        
+        stds_phone = request.POST.get("std_phone")   
+
+        #creating object for Student model and adding fetched inputs into table
+        std = Student()
+        std.roll = stds_roll
+        std.name = stds_name
+        std.email = stds_email
+        std.address = stds_address
+        std.phone = stds_phone
+
+        # save object to add info in table
+        std.save()
+        return redirect("/student/home")
+
     return render(request, 'addStudent.html')
